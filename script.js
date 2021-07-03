90671affc4632797eb0f9ec5fd5565027e61e208
McCambley <jakemccamb16@gmail.com>
2021-07-03 01:09:53 -0400

    Fix: make each function do only one thing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const page = document.querySelector(".page");
-const toggle = page.querySelector(".toggle-input");
-const toggleIcon = page.querySelector(".toggle-icon");
+const page = document.querySelector('.page');
+const toggle = page.querySelector('.toggle-input');
+const toggleIcon = page.querySelector('.toggle-icon');
 
 // set theme and localStorage on page load
 setCheckedState();
@@ -9,7 +9,7 @@ function setCheckedState() {
   // checks if localStorage has a "checked" value set at all
   if (!(localStorage.checked === undefined)) {
     // if it does, it sets the state of the toggle accordingly
-    toggle.checked = isTrue(localStorage.getItem("checked"));
+    toggle.checked = isTrue(localStorage.getItem('checked'));
     // after setting the toggle state, the theme is adjusted according to the checked state
     toggleTheme();
   }
@@ -17,30 +17,38 @@ function setCheckedState() {
 
 function toggleTheme() {
   // Toggle theme based on state of checkbox
-  if (toggle.checked) {
-    page.classList.replace("light", "dark");
-  } else {
-    page.classList.replace("dark", "light");
-  }
+  replaceClass();
   // replace icons on page
   toggleIconTheme();
   // set the value of the "checked" key in localStorage
-  localStorage.setItem("checked", toggle.checked);
+  updateLocalStorage();
+}
+
+function replaceClass() {
+  if (toggle.checked) {
+    page.classList.replace('light', 'dark');
+  } else {
+    page.classList.replace('dark', 'light');
+  }
 }
 
 function toggleIconTheme() {
   // Replace icons not able to be targeted by css variables
-  if (page.classList.contains("light")) {
-    toggleIcon.src = "./images/moon.svg";
+  if (page.classList.contains('light')) {
+    toggleIcon.src = './images/moon.svg';
   } else {
-    toggleIcon.src = "./images/sun.svg";
+    toggleIcon.src = './images/sun.svg';
   }
+}
+
+function updateLocalStorage() {
+  localStorage.setItem('checked', toggle.checked);
 }
 
 function isTrue(value) {
   // convert string to boolean
-  return value === "true";
+  return value === 'true';
 }
 
 // Toggle theme any time the state of the checkbox changes
-toggle.addEventListener("change", toggleTheme);
+toggle.addEventListener('change', toggleTheme);
